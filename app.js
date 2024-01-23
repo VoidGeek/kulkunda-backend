0a1544f342a30a808413cdecee61e500f95c4431
VoidGeek <pradyumna.p.6969@gmail.com>
2024-01-23 21:57:32 +0530

    cors issue

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const userrouter = require('./routes/userrouter.js');
-const contactRoutes = require('./routes/contactRoute'); // Import the contact routes
-const sevaRoutes = require('./routes/sevaRoute'); // Import the seva routes
-const sevalistRoutes = require('./routes/sevalistroute'); // Import the sevalist routes
-const newsupdateRoutes = require('./routes/newsupdatesroute'); // Import the newsupdate routes
-const galleryRoutes = require('./routes/galleryRoute'); // Import the newsupdate routes
+const contactRoutes = require('./routes/contactRoute');
+const sevaRoutes = require('./routes/sevaRoute');
+const sevalistRoutes = require('./routes/sevalistroute');
+const newsupdateRoutes = require('./routes/newsupdatesroute');
+const galleryRoutes = require('./routes/galleryRoute');
 
 app.use(express.json());
 app.use(cookieParser());
 
+const corsOptions = {
+  origin: 'http://seva.basavanamoolatemple.in', // Replace with your actual allowed origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
+app.use(cors(corsOptions)); // Use cors middleware with options
 
-app.use('/api/user', userrouter); // Mount the user routes
-app.use('/api/contact', contactRoutes); // Mount the contact form routes
-app.use('/api/seva', sevaRoutes); // Mount the seva routes
-app.use('/api/sevalist', sevalistRoutes); // Mount the sevalist routes
-app.use('/api/newsupdate', newsupdateRoutes); // Mount the newsupdate routes
-app.use('/api/gallery', galleryRoutes); // Mount the gallery routes
+app.use('/api/user', userrouter);
+app.use('/api/contact', contactRoutes);
+app.use('/api/seva', sevaRoutes);
+app.use('/api/sevalist', sevalistRoutes);
+app.use('/api/newsupdate', newsupdateRoutes);
+app.use('/api/gallery', galleryRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
